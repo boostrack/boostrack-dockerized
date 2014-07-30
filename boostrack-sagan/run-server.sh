f212632b741c8e2833c1168666aa5649223dcd8e
boostrack <hflambo@gmail.com>
2014-07-30 16:57:44 +0200

    Delete run-server.sh

--- boostrack-sagan/run-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-NAME="boostrack-sagan"
-
-IMAGE_ID=$(docker ps -a | grep $NAME | cut -f1 -d' ')
-[ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Container: [$NAME] removed!! IMAGE_ID: $IMAGE_ID"
-
-docker run --name $NAME -d -p 3333:8080 -t $NAME
